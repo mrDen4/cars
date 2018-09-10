94ed46710f115e558e2a3fd5d95499fd986fafd5
Denis Davydov <den.saint.k@gmail.com>
2018-09-10 12:28:59 +0300

    add some css

--- src/js/main.js
+++ src/js/main.js
@@ -1,63 +1,50 @@
 $(document).ready(function () {
-    //Кнопка заказать звонок
-    $('.header__burger').on('click', function () {
-        $('.menu').toggleClass('menu--active');
+    $('.talk__slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: $('.talk__arrow-left'),
+        nextArrow: $('.talk__arrow-right'),
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1
+              }
+            }
+          ]
     });
 
-    $('.item__link').on('click', function () {
-        $('.menu').removeClass('menu--active');
-    });
-
-    $('.menu__exit').on('click', function () {
-        $('.menu').toggleClass('menu--active');
-    });
-
-    //Кнопка записаться на консультацию
-    $('.hero__btn').on('click', function () {
-        $('.modal__card').toggleClass('modal__card--active');
-        $('.bg__modal').toggleClass('bg__modal--active');
-    });
-
-    //Кнопка записаться на консультацию
-    $('.bonus__btn').on('click', function () {
-        $('.modal__card-bonus').toggleClass('modal__card-bonus--active');
-        $('.bg__modal').toggleClass('bg__modal--active');
-    });
-
-    //Выход из popup
-    $('.bg__modal').on('click', function () {
-        $('.modal__card').removeClass('modal__card--active');
-        $('.modal__card-bonus').removeClass('modal__card-bonus--active');
-        $('.bg__modal').removeClass('bg__modal--active');
-    });
-
-    $('.modal__exit').on('click', function () {
-        $('.modal__card').removeClass('modal__card--active');
-        $('.modal__card-bonus').removeClass('modal__card-bonus--active');
-        $('.bg__modal').removeClass('bg__modal--active');
-    });
-
-    //Отправка данных пользователя на mail
-    $("#callback-form").submit(function () {
-        $.ajax({
-            type: "POST",
-            url: "assets/php/mail.php",
-            data: $(this).serialize()
-        }).done(function () {
-            $('.form__complete').toggleClass('form__complete--active');
-        });
-        return false;
-    });
-
-    $("#popup-form").submit(function () {
-        $.ajax({
-            type: "POST",
-            url: "assets/php/mail.php",
-            data: $(this).serialize()
-        }).done(function () {
-            $('.form__complete').toggleClass('form__complete--active');            
-        });
-        return false;
+    $('.cars__slider').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: $('.cars__arrow-left'),
+        nextArrow: $('.cars__arrow-right'),
+        responsive: [
+            {
+              breakpoint: 1000,
+              settings: {
+                slidesToShow: 3
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1
+              }
+            }
+          ]
     });
 });
 
