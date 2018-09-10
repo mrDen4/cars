1bb70eeb796490095d93b9cb86a16c4166ff1e37
Denis Davydov <den.saint.k@gmail.com>
2018-09-10 17:26:10 +0300

    add some changes and add menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,50 +1,65 @@
 $(document).ready(function () {
-    $('.talk__slider').slick({
-        infinite: true,
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        prevArrow: $('.talk__arrow-left'),
-        nextArrow: $('.talk__arrow-right'),
-        responsive: [
-            {
-              breakpoint: 768,
-              settings: {
-                slidesToShow: 1
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1
-              }
-            }
-          ]
-    });
+  $('.header__btn').on('click', function () {
+    $('.menu').toggleClass('menu--active');
+    $('.bg__modal').toggleClass('bg__modal--active');
+  });
 
-    $('.cars__slider').slick({
-        infinite: true,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        prevArrow: $('.cars__arrow-left'),
-        nextArrow: $('.cars__arrow-right'),
-        responsive: [
-            {
-              breakpoint: 1000,
-              settings: {
-                slidesToShow: 3
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1
-              }
-            }
-          ]
-    });
+  $('.menu__exit').on('click', function () {
+    $('.menu').removeClass('menu--active');
+    $('.bg__modal').removeClass('bg__modal--active');
+  });
+
+  $('.bg__modal').on('click', function () {
+    $('.menu').removeClass('menu--active');
+    $('.bg__modal').removeClass('bg__modal--active');
+  });
+  
+  $('.talk__slider').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: $('.talk__arrow-left'),
+    nextArrow: $('.talk__arrow-right'),
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+  $('.cars__slider').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: $('.cars__arrow-left'),
+    nextArrow: $('.cars__arrow-right'),
+    responsive: [
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 });
 
